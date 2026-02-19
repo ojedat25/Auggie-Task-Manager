544a8c693f95a9bde7010352fdc40ce957fb7234
Toni Ojeda <toniojeda2015@gmail.com>
2026-02-19 14:51:09 -0600

    feat: update extract_calendar_data function to return calendar object from Moodle

--- backend/auggietaskmanager/moodle/utils.py
+++ backend/auggietaskmanager/moodle/utils.py
@@ -1,5 +1,6 @@
 # Put all helper functions here
 import requests
+from icalendar import Calendar
 
 
 def extract_calendar_data():
@@ -7,9 +8,15 @@ def extract_calendar_data():
 
     Args:
         calendar_url (String): moodle calendar url
+    Returns:
+        Calendar: calendar object containing all the events in the calendar
     """
     calendar_url = "https://moodle.augsburg.edu/moodle2021/calendar/export_execute.php?userid=12513&authtoken=79c6051c6291eedd78af084112b043c2a57532cc&preset_what=all&preset_time=custom"
+
     response = requests.get(calendar_url)
 
-    return response.text
+    # Replace tab-based folded lines with space-based folded lines
 
+    calendar = Calendar.from_ical(response.content)
+
+    return calendar
