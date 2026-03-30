2af387a6920ab26b06b66f1f6eeb0865b5056129
toni-sureattend <ojedat@augsburg.edu>
2026-03-30 13:29:13 -0500

    Added add task button and created the TaskFormModal.tsx file to start building the form for when a user wants to add a new task.

---
+++ frontend/AuggieTaskManagerFrontend/src/renderer/features/tasks/components/TaskFormModal.tsx
@@ -0,0 +1,10 @@
+
+
+
+export const TaskFormModal = () => {
+  return (
+    <div>
+      <h1>Task Form Modal</h1>
+    </div>
+  );
+};
--- frontend/AuggieTaskManagerFrontend/src/renderer/features/tasks/components/TaskList.tsx
+++ frontend/AuggieTaskManagerFrontend/src/renderer/features/tasks/components/TaskList.tsx
@@ -1,5 +1,6 @@
 import { TaskCard } from './TaskCard';
 import { Task } from '../../../types/task';
+import { Plus } from 'lucide-react';
 
 export interface TaskListProps {
   tasks: Task[];
@@ -9,7 +10,7 @@ export const TaskList = ({ tasks }: TaskListProps) => {
   return (
     <div>
       <ul className="list bg-base-100 rounded-box shadow-md">
-        <li className="p-4 pb-2 text-xl opacity-60 tracking-wide">Tasks</li>
+        <li className="p-4 pb-2 text-xl opacity-60 tracking-wide"><span className="flex items-center gap-2">Tasks</span></li>
         {tasks.map((task, index) => (
           <li className="list-row" key={task.id}>
             <div className="w-8 shrink-0 text-right text-lg font-thin opacity-30 tabular-nums">
