055215f7762c2e565f0302b06618cdcfaa17d328
Dustin Davidson <davidsod@augsburg.edu>
2026-04-02 10:21:00 -0500

    Added to fields to the groupStudy model, Now references soon to be created courses model

--- backend/auggietaskmanager/groups/models.py
+++ backend/auggietaskmanager/groups/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from moodle.models import Courses
 
 # Create your models here.
 class StudyGroup(models.Model):
+    groupID = models.AutoField(primary_key=True)
+    
     name = models.CharField(max_length=100)
 
     description = models.TextField(max_length=500, blank=True)
 
+    image = models.ImageField(upload_to='study_groups/', blank=True, null=True)
+
+    private = models.BooleanField(default=False)
+
     members = models.ManyToManyField(User, related_name='study_groups')
+
+    related_courses = models.ManyToManyField(Courses, related_name='study_groups')
 
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_groups')
 
